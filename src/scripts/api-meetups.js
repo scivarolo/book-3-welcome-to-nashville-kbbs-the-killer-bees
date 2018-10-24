414f01ffb1242a1fbace2a0b0edf52da55b76606
kellymorin <kmorin06@gmail.com>
2018-10-24 16:01:40 -0500

    End of day 1, resolved API challenges with owned database.

--- src/scripts/api-meetups.js
+++ src/scripts/api-meetups.js
@@ -1,26 +1,43 @@
 // API Fetch Commands
-fetch(myapi) //Need to update to API and include key
-.then((fooddata)= fooddata.json()) //Need to update names
-.then((realdata)=>{ //Need to update
-  realdata.forEach((food)=>{ // Need to update, loop over data and return to function
-    localFood = food
-    fetch(theirapi)
-    .then((apiFoodItem)=> apiFoodItem.json())
-    .then((convertedFood => {
-      let fragment = document.createDocumentFragment()
-      let title = elementFactory ("h3", food.name, {
-        id: null,
-        class: "foodTitle"
-      })
-      let ingredients = elementFacotry ("p", convertedFood.product.ingredients_text, {
-        id: null,
-        class: "foodIngredients"
-      })
-      let foodListItem = elementFactory ("li", null, {id: "foodItem", clazz: "foodItem"}, title, ingredients)
-
-      fragment.appendChild(foodListItem)
-
-      foodList.appendChild (fragment)
-    }))
+let allEvents = []
+let eventGroup = []
+let fragment = document.createDocumentFragment()
+fetch("http://localhost:8088/events")
+.then(eventdata => eventdata.json())
+.then((events)=>{
+  events.forEach((event)=>{ // Need to update, loop over data and return to function
+    allEvents.push(event);
   })
+  // console.log(allEvents)
+  return allEvents
 })
+    // eventGroup.push(event.group);
+    // let eventResult = elementFacotry("li", event.name, event.group.name, {
+    //     id: something,
+    //     clazz: something
+    //   })
+  // let meetupEvents = elementFactory("ul", null, {
+  //   id: something,
+  //   clazz: something
+  // }, eventResult)
+
+
+  // let title = elementFactory ("h3", food.name, {
+  //   id: null,
+  //   class: "foodTitle"
+  // })
+  // let ingredients = elementFacotry ("p", convertedFood.product.ingredients_text, {
+  //   id: null,
+  //   class: "foodIngredients"
+  // })
+  // let foodListItem = elementFactory ("li", null, {id: "foodItem", clazz: "foodItem"}, title, ingredients)
+  // let results = elementFactory("ul", null, {
+  //   id: null,
+  //   clazz: "something"
+  // })
+
+  // fragment.appendChild(foodListItem)
+
+  // foodList.appendChild (fragment)
+// Topic Category[Outdoors & Adventure, Tech, Family, Health & Wellness, Sports & Fitness, ]
+
--- src/scripts/meetup.js
+++ src/scripts/meetup.js
@@ -1,11 +1,40 @@
 // Eventlistener on button, takes input and calls function to fetch data from API, pass in parameter with search terms
 
+// Meetup input button id="meetupInputBtn"
+
 // Takes data from API and makes a new element
+// Add element factory code
+// let elementFactory = (el, content, {id, clazz}, ...children)=>{
+//   let element = document.createElement(el)
+//   element.innerHTML = content || null
+//   children.forEach(child => {
+//     element.appendChild(child)
+//   })
+//   element.setAttribute("id", id)
+//   element.setAttribute("class", clazz)
+//   return element
+// }
 
 // Display element in results portion of the DOM
+// DOM section is class="results__meetups"
+
+
+let meetupResults = (events) =>{
+  let fragment = document.createDocumentFragment()
+  let meetupEvents = elementFactory("ul", null, null, null, {clazz: "meetup__allResults"})
+  events.forEach((event)=>{
+    let eventResult = elementFactory("li", event.name, event.group.name, event.local_date, {clazz: "meetup__result"})
+    fragment.appendChild(eventResult)
+    // return eventResult
+  })
+  selectora.appendChild(fragment)
+}
 
 // Event listener on results portion of the DOM that takes selected element and passes it to itenary section
 
+
 // Display selected element in itenary portion of the DOM
+// DOM section is class="itinerary__meetups"
 
 // Void additional inputs from that section
+// DOM section is class="search__meetups"
