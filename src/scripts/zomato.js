d80688f7bb8b15010c59fdc53ed6d999f748e2c6
Sebastian Civarolo <scivarolo@gmail.com>
2018-10-24 23:02:18 -0500

    Write function to capture user input for Zomato search

--- src/scripts/zomato.js
+++ src/scripts/zomato.js
@@ -1,10 +1,18 @@
-// Create a function to take the input from the Zomato input field and convert it to a url to pass to the zomatoAPI function
+const zomatoSearchBtn = document.querySelector("#zomatoInputBtn");
+const zomatoInput = document.querySelector("#zomatoInput");
 
+// This function captures the input from the Zomato search and converts it to a query string with +'s instaed of spaces.
+function getZomatoSearch() {
+  let queryString = zomatoInput.value;
+  queryString = queryString.split(" ").join("+");
+  return queryString;
+}
 
+// Add Event Listener to Zomato search button
+zomatoSearchBtn.addEventListener("click", getZomatoSearch);
 
 // Create a function output the results from Zomato into a list on the DOM
 
-// Add Event Listener to search button
 
 // Add Event Listener to Add to Itinerary Button
 
