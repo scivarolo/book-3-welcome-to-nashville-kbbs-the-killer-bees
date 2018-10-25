fe947a045d172707f92fdcbf694bfb02a01aa12e
Sebastian Civarolo <scivarolo@gmail.com>
2018-10-24 23:01:24 -0500

    Rewrite zomatoAPI.request method with queryString as parameter instead of url.

--- src/scripts/api-zomato.js
+++ src/scripts/api-zomato.js
@@ -1,13 +1,15 @@
 const zomatoAPI = {
-  defaultUrl: "https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=city&q=hot%2Bchicken",
-  request: function (url) {
+  defaultUrl: "https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=city&q=hot+chicken",
+  baseUrl: "https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=city&q=",
+  request: function (queryString) {
+    let url = this.baseUrl + queryString;
     return fetch(url, {
       headers: {
         "user-key": "4a3b2c80da683a394fed9954095ed6fd"
       }
-    }).then(response => response.json())
+    }).then(response => response.json());
   }
-}
+};
 
-zomatoAPI.request(zomatoAPI.defaultUrl)
+zomatoAPI.request("tacos")
   .then(results => console.log(results));
