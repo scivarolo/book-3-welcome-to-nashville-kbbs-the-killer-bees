8781d38b6801d981b555a35405eed2ca55a44151
Sebastian Civarolo <scivarolo@gmail.com>
2018-10-26 15:40:25 -0500

    Make results hide when switching tabs, and display when you go back to the tab

--- src/scripts/interaction.js
+++ src/scripts/interaction.js
@@ -13,17 +13,22 @@ function switchSearchTabs(clickEvent) {
   searchNav.forEach(tab => tab.classList.remove("active"));
   clickedTab.classList.add("active");
   searchSections.forEach(searchSection => searchSection.classList.remove("show"));
+  document.querySelectorAll("[class^='results__']").forEach(section => section.classList.remove("show"));
 
   if (clickedTab.textContent === "Restaurants") {
     searchSections[0].classList.add("show");
+    document.querySelector(".results__zomato").classList.add("show");
   }
-  if (clickedTab.textContent === "Concerts") {
+  if (clickedTab.textContent === "Music/Sports") {
     searchSections[1].classList.add("show");
+    document.querySelector(".results__songkick").classList.add("show");
   }
   if (clickedTab.textContent === "Parks") {
     searchSections[2].classList.add("show");
+    document.querySelector(".results__parks").classList.add("show");
   }
   if (clickedTab.textContent === "Events") {
     searchSections[3].classList.add("show");
+    document.querySelector(".results__eventbrite").classList.add("show");
   }
 }
