6e7e6448d7c0dce2b212c5412abeac85ea3c93d3
kellymorin <kmorin06@gmail.com>
2018-10-28 18:21:37 -0500

    beginning work on welcome page popup.

--- src/scripts/welcome.js
+++ src/scripts/welcome.js
@@ -13,8 +13,35 @@ function makeWelcomePage (){
 }
 makeWelcomePage();
 
-// Create event listener that loads welcome page upon page load
+// I need to add an event listener that when some section of the page loads, it calls this function to create my popup.
 
+// Create event listener that loads welcome page upon page load
+// function popup(mylink, windowname) {
+//   if (! window.focus)return true;
+//   var href;
+//   if (typeof(mylink) == 'string')
+//   href=mylink;
+//   else href=mylink.href;
+//   window.open(href, windowname, 'width=400,height=200,scrollbars=yes');
+//   return false; }
+
+// onClick = myFunction
+
+// function showWelcome (){
+//   welcomePage.classList.toggle("show")
+// }
+
+let body = document.querySelector("body");
+console.log(body)
+
+body.addEventListener("onLoad", ()=>{
+  console.log("the event listener has been added")
+  welcomePage.classList.toggle("show")
+})
+// function myFunction(){
+//   var popup = get element by id("myPopup")
+//   popup.classList.toggle("show")
+// }
 // Create event listner on button click that captures the input and passes it into a variable
 
 // Capture input and pass to JSON and other portions of page
