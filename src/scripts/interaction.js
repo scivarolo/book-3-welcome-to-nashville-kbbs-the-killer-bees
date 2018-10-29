69557c80d8bbea9ca26e6795ec6a3e0bc8441fda
Sebastian Civarolo <scivarolo@gmail.com>
2018-10-28 20:59:24 -0500

    Swap out header images on tab change.

--- src/scripts/interaction.js
+++ src/scripts/interaction.js
@@ -1,6 +1,7 @@
-let searchNav = document.querySelectorAll(".search-tab");
-let searchSections = document.querySelectorAll("[class^='search__']");
-let searchHeading = document.querySelector(".search-heading");
+const searchNav = document.querySelectorAll(".search-tab");
+const searchSections = document.querySelectorAll("[class^='search__']");
+const searchHeading = document.querySelector(".search-heading");
+const welcomeHeader = document.querySelector(".header__div img");
 console.log(searchSections);
 searchNav.forEach(tab => {
   tab.addEventListener("click", (event) => {
@@ -10,30 +11,32 @@ searchNav.forEach(tab => {
 
 function switchSearchTabs(clickEvent) {
   let clickedTab = clickEvent.target;
-
   searchNav.forEach(tab => tab.classList.remove("active"));
   clickedTab.classList.add("active");
   searchSections.forEach(searchSection => searchSection.classList.remove("show"));
   document.querySelectorAll("[class^='results__']").forEach(section => section.classList.remove("show"));
 
+
   if (clickedTab.textContent === "Restaurants") {
     searchSections[0].classList.add("show");
     document.querySelector(".results__zomato").classList.add("show");
-    // searchHeading.textContent = "Find Somewhere to Eat";
+    welcomeHeader.setAttribute("src", "img/kbbs-restaurants-header.jpg");
   }
   if (clickedTab.textContent === "Music/Sports") {
     searchSections[1].classList.add("show");
     document.querySelector(".results__songkick").classList.add("show");
-    // searchHeading.textContent = "Find a Show or Sporting Event";
+    welcomeHeader.setAttribute("src", "img/kbbs-music-sports-header.jpg");
   }
   if (clickedTab.textContent === "Parks") {
     searchSections[2].classList.add("show");
     document.querySelector(".results__parks").classList.add("show");
+    welcomeHeader.setAttribute("src", "img/kbbs-parks-header.jpg");
     // searchHeading.textContent = "Find a Park";
   }
   if (clickedTab.textContent === "Events") {
     searchSections[3].classList.add("show");
     document.querySelector(".results__eventbrite").classList.add("show");
+    welcomeHeader.setAttribute("src", "img/kbbs-events-header.jpg");
     // searchHeading.textContent = "Find an Event";
   }
 }
